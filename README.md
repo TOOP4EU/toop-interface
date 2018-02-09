71b4ca378b1bac9f590e3cb5bfe48cfcc069bd65
Anton Wiklund <ixuz07@gmail.com>
2018-02-09 13:02:25 +0100

    A sample rest server using Spark

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # toop-dc-interface
+
+mvn clean compile assembly:single && java -jar target/toop-dc-interface-1.0.0-SNAPSHOT-jar-with-dependencies.jar
--- src/main/java/eu/toop/dc/DCInterface.java
+++ src/main/java/eu/toop/dc/DCInterface.java
@@ -1,5 +1,9 @@
 package eu.toop.dc;
 
+import static spark.Spark.*;
+
 public class DCInterface {
-    
+    public static void main(String[] args) {
+        get("/dc-data-response", (req, res) -> "An endpoint that receives data");
+    }
 }
