8bbf1b6cfa4380526f0ba923bf0b8784e4dd232d
Philip Helger <philip@helger.com>
2018-02-12 10:22:50 +0100

    Removed Spark

--- src/main/java/eu/toop/iface/MainInterface.java
+++ src/main/java/eu/toop/iface/MainInterface.java
@@ -1,9 +1,9 @@
 package eu.toop.iface;
 
-import static spark.Spark.*;
-
-public class MainInterface {
-    public static void main(String[] args) {
-        get("/dc-data-response", (req, res) -> "An endpoint that receives data");
-    }
+public class MainInterface
+{
+  public static void main (final String [] args)
+  {
+    System.out.println ("Main");
+  }
 }
