dc25b4d4b150720b7f7bebf9fed26a53e2313ada
Philip Helger <philip@helger.com>
2018-02-13 21:57:04 +0100

    Added dummy httpclient call

---
+++ src/main/java/eu/toop/iface/mockup/client/SendToMPClient.java
@@ -0,0 +1,34 @@
+package eu.toop.iface.mockup.client;
+
+import java.io.IOException;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.w3c.dom.Document;
+
+import com.helger.httpclient.HttpClientFactory;
+import com.helger.httpclient.HttpClientManager;
+import com.helger.httpclient.response.ResponseHandlerXml;
+
+public final class SendToMPClient {
+	private SendToMPClient() {
+	}
+
+	public static void httpClientCall(final byte[] aDataTosend) throws IOException {
+		final HttpClientFactory aHCFactory = new HttpClientFactory();
+		// For proxy etc
+		aHCFactory.setUseSystemProperties(true);
+
+		try (final HttpClientManager aMgr = new HttpClientManager(aHCFactory)) {
+			final HttpPost aPost = new HttpPost("http://example.org");
+			aPost.setEntity(new ByteArrayEntity(aDataTosend));
+
+			// Assume response is XML
+			final ResponseHandlerXml aRH = new ResponseHandlerXml();
+			final Document aResponse = aMgr.execute(aPost, aRH);
+			if (aResponse != null) {
+				// TODO
+			}
+		}
+	}
+}
